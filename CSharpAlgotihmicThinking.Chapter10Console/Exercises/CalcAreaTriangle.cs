585983defe0393f3a201e7c52a6fdb8c967e4af9
George Pikoulas <pikoulas@fortunethellas.gr>
2020-11-13 14:26:37 +0200

    CalcAreaTriangle

---
+++ CSharpAlgotihmicThinking.Chapter10Console/Exercises/CalcAreaTriangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAlgotihmicThinking.Chapter10Console.Exercises
+{
+    public class CalcAreaTriangle
+    {
+        public void Calculate()
+        {
+            double baseArea, height, area;
+            Console.Write("Please enter the base of the triangle");
+            baseArea = double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the height of the triangle");
+            height = double.Parse(Console.ReadLine());
+
+
+            area = (baseArea * height) / 2;
+
+            Console.WriteLine("The area is " + area);
+
+        }
+    }
+}
